d7d567a19f535aa70adb365ac16021ccbc038d94
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-23 11:26:43 +0300

    name updated

--- WhatsappClone/ios/WhatsappCloneTests/WhatsappCloneTests.m
+++ WhatsappClone/ios/WhatsappCloneTests/WhatsappCloneTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WhatsappCloneTests : XCTestCase
+@interface whatsappcloneTests : XCTestCase
 
 @end
 
-@implementation WhatsappCloneTests
+@implementation whatsappcloneTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
