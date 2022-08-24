cf336834af83bcb39c7b8e3dfb58e93aeeedcd46
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-24 15:45:40 +0300

    installed react-native-vector-icons  & progress made in topnavbar.js

--- WhatsappClone/src/components/TopNavbar/TopNavbar.js
+++ WhatsappClone/src/components/TopNavbar/TopNavbar.js
@@ -1,12 +1,20 @@
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import React from 'react';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import styles from './TopNavbar.style';
 
 const TopNavbar = () => {
   return (
-    <View style={styles.Nav}>
-      <Text>Whatsapp</Text>
+    <View style={styles.topNavbarWrapper}>
+      <View style={{ flex: 1 }}>
+        <Text style={styles.topNavbarText}>Whatsapp</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <AntDesign name="home" color="white" size={30} />
+        <Entypo name="home" color="white" size={30} />
+      </View>
     </View>
   );
 };
--- WhatsappClone/src/components/TopNavbar/TopNavbar.style.js
+++ WhatsappClone/src/components/TopNavbar/TopNavbar.style.js
@@ -1,3 +1,15 @@
-import {StyleSheet} from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({});
+import { theme } from '../../../utils';
+
+export default StyleSheet.create({
+  topNavbarWrapper: {
+    height: 50,
+    backgroundColor: theme.colors.foreground,
+    // justifyContent: 'flex-end',
+  },
+  topNavbarText: {
+    color: theme.colors.secondaryText,
+  },
+});
