0e7d5556b021ce232c4b171564dd1c7ce4274b9b
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-25 10:08:37 +0300

    TabNavigator created to simplify App.js, carried one return

--- WhatsappClone/src/App.js
+++ WhatsappClone/src/App.js
@@ -1,27 +1,19 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import TopNavbar from './components/TopNavbar';
-import Chats from './pages/Chats';
-import Status from './pages/Status';
-import Calls from './pages/Calls';
-import Context from '../context/Context';
 import ContextWrapper from '../context/ContextWrapper';
+import TabNavigator from './TabNavigator';
 
-const Tab = createMaterialTopTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const {
-    theme: { colors },
-  } = React.useContext(Context);
   return (
     <ContextWrapper>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ header: TopNavbar }}>
-          <Stack.Screen name="home" component={Home} />
+          <Stack.Screen name="home" component={TabNavigator} />
           {/* <Stack.Screen
             name="home"
             options={{ title: 'Whatsapp' }}
@@ -32,33 +24,5 @@ const App = () => {
     </ContextWrapper>
   );
 };
-
-function Home() {
-  const {
-    theme: { colors },
-  } = React.useContext(Context);
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarIndicatorStyle: {
-          backgroundColor: colors.text,
-          height: 4,
-        },
-        tabBarStyle: {
-          backgroundColor: colors.foreground,
-        },
-        tabBarLabelStyle: {
-          fontWeight: '600',
-        },
-        tabBarActiveTintColor: colors.text,
-        tabBarInactiveTintColor: colors.secondaryText,
-      }}
-      initialRouteName="chats">
-      <Tab.Screen name="Chats" component={Chats} />
-      <Tab.Screen name="Status" component={Status} />
-      <Tab.Screen name="Calls" component={Calls} />
-    </Tab.Navigator>
-  );
-}
 
 export default App;
---
+++ WhatsappClone/src/TabNavigator.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import Chats from './pages/Chats';
+import Status from './pages/Status';
+import Calls from './pages/Calls';
+import Context from '../context/Context';
+
+const Tab = createMaterialTopTabNavigator();
+
+const TabNavigator = () => {
+  const {
+    theme: { colors },
+  } = React.useContext(Context);
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.text,
+          height: 4,
+        },
+        tabBarStyle: {
+          backgroundColor: colors.foreground,
+        },
+        tabBarLabelStyle: {
+          fontWeight: '600',
+        },
+        tabBarActiveTintColor: colors.text,
+        tabBarInactiveTintColor: colors.secondaryText,
+      }}
+      initialRouteName="chats">
+      <Tab.Screen name="Chats" component={Chats} />
+      <Tab.Screen name="Status" component={Status} />
+      <Tab.Screen name="Calls" component={Calls} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
