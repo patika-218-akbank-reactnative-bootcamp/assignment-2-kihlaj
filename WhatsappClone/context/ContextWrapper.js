1ad6c9ae30aee0af824807e28cd266d34a7137f5
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-26 23:48:13 +0300

    deleted unused parts

--- WhatsappClone/context/ContextWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Context from './Context';
-import { theme } from '../utils';
-
-export default function ContextWrapper(props) {
-  return (
-    <Context.Provider value={{ theme }}>{props.children}</Context.Provider>
-  );
-}
--- WhatsappClone/src/App.js
+++ WhatsappClone/src/App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ContextWrapper from '../context/ContextWrapper';
 import TabNavigator from './TabNavigator';
 import Chat from './pages/Chat';
 import { theme } from '../utils';
@@ -13,52 +12,48 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <ContextWrapper>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            // to remove shadow between tab navigation and header
-            headerShadowVisible: false,
-            headerStyle: {
-              backgroundColor: theme.colors.foreground,
-            },
-            headerTintColor: theme.colors.text,
-            headerTitleAlign: 'left',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}>
-          <Stack.Screen
-            name="Home"
-            component={TabNavigator}
-            options={{
-              title: 'WhatsApp',
-              headerRight: () => (
-                <TopNavbar
-                  firstIcon="search1"
-                  secondIcon="dots-three-vertical"
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="Chat"
-            component={Chat}
-            options={({ route }) => ({
-              // to display the title by passing parameters to the route
-              headerTitle: route.params.name,
-              headerRight: () => (
-                <ChatNavbar
-                  firstIcon="videocamera"
-                  secondIcon="phone"
-                  thirdIcon="dots-three-vertical"
-                />
-              ),
-            })}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ContextWrapper>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          // to remove shadow between tab navigation and header
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: theme.colors.foreground,
+          },
+          headerTintColor: theme.colors.text,
+          headerTitleAlign: 'left',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          // tabs below header listed inside TabNavigator
+          component={TabNavigator}
+          options={{
+            title: 'WhatsApp',
+            headerRight: () => (
+              <TopNavbar firstIcon="search1" secondIcon="dots-three-vertical" />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+          options={({ route }) => ({
+            // to display the title by passing parameters to the route
+            headerTitle: route.params.name,
+            headerRight: () => (
+              <ChatNavbar
+                firstIcon="videocamera"
+                secondIcon="phone"
+                thirdIcon="dots-three-vertical"
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
